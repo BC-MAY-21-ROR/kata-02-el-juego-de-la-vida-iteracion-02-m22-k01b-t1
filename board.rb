484f2deeda3185bf5d12f48aaca168db77ad9eb9
ruruisu <zaddozgl@gmail.com>
2022-05-11 19:11:10 -0500

    added a neighbour method to detect nearby cells Co-authored-by: acorzo alexecorzo@gmail.com Co-authored-by: guillermo-delprado galfonso.delprado@gmail.com

--- board.rb
+++ board.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require './cell.rb'
+
+require './cell'
 require 'matrix'
 # Class for the life grid
 class Board
@@ -8,26 +9,58 @@ class Board
   def initialize(rows, columns)
     @rows = rows
     @columns = columns
+
+    # @cells_matrix = Array.new(rows) { Array.new(columns) { [' . '] } }
+    @cells_matrix = Matrix.build(rows, columns) {0}
+  end
+
+  # def position_cell(cell); 
+  #   cells_matrix.find_index(cell)
+  # end
+  # def print_matrix
+  #   i = 0
+  #   while i < rows
+  #     puts @cells_matrix[i].join(' ')
+  #     i += 1
+  #   end
+  # end
+
+  def initial_position
+    @cells_matrix[1,4] = 1
+    @cells_matrix[2,4] = 1
+    @cells_matrix[2,3] = 1
+    @cells_matrix[3,6] = "x"
     
-    @cells_matrix = Array.new(rows){Array.new(columns){[" . "]}}
   end
 
-  def print_matrix
-    i = 0
-    while i < rows
-      puts @cells_matrix[i].join(" ")
-      i = i+1
-    end
-  end
+  def neighbours?(cell)
+    neighbours = []
+    x = (cells_matrix.find_index(cell)[0])
+    y = (cells_matrix.find_index(cell)[1])
 
-  def initial_position 
-    @cells_matrix[1][4] = " * "
-    @cells_matrix[2][4] = " * "
-    @cells_matrix[2][3] = " * "
+    neighbours.<<(cells_matrix[x - 1, y - 1])
+
+    neighbours.<<(cells_matrix[x - 1, y])
+
+    neighbours.<<(cells_matrix[x - 1, y + 1])
+
+    neighbours.<<(cells_matrix[x, y - 1])
+
+    neighbours.<<(cells_matrix[x, y + 1])
+
+    neighbours.<<(cells_matrix[x + 1, y - 1])
+
+    neighbours.<<(cells_matrix[x + 1, y])
+
+    neighbours.<<(cells_matrix[x + 1, y + 1])
+
+    puts neighbours
+    neighbours.sum
   end
 end
 
 board1 = Board.new(4, 8)
 
 board1.initial_position
-board1.print_matrix
+puts board1.cells_matrix
+puts board1.neighbours?(board1.cells_matrix[3,7])
--- cell.rb
+++ cell.rb
@@ -1,16 +1,17 @@
-class Cell 
+# frozen_string_literal: true
+
+# class for representing the cellular organism
+class Cell
   attr_accessor(:stage, :neighbours)
-  
-  def initialize(stage, neighbours)
-    @stage = stage 
+
+  def initialize(stage)
+    @stage = stage
     @neighbours = neighbours
 
     @live = false
   end
-  
-  def position_cell(cell)
-    
-  end
+
+  def position_cell(cell); end
 
   def dead?
     !@live
@@ -24,19 +25,5 @@ class Cell
     @live
   end
 
-  def neighbours?
-    neighbours = []
-    neighbours.<<(@board.cell(self.x - 1, self.y - 1))
-    neighbours.<<(@board.cell(self.x - 1, self.y))
-    neighbours.<<(@board.cell(self.x - 1, self.y + 1))
 
-    neighbours.<<(@board.cell(self.x, self.y - 1))
-    neighbours.<<(@board.cell(self.x, self.y + 1))
-
-    neighbours.<<(@board.cell(self.x + 1, self.y - 1))
-    neighbours.<<(@board.cellt(self.x + 1, self.y))
-    neighbours.<<(@board.cell(self.x + 1, self.y + 1))
-
-    neighbours
-  end
 end
